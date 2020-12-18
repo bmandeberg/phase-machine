4656e34ba149c2244885887695fcfead0dc829c1
Ben Mandeberg <bmandeberg@ucla.edu>
2020-12-17 21:11:21 -0800

    Range view snapping to correct keys

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -169,6 +169,7 @@ export default function Channel({
           setRangeStart={setRangeStart}
           rangeEnd={rangeEnd}
           setRangeEnd={setRangeEnd}
+          channelNum={channelNum}
         />
       </div>
     )
--- src/components/PianoRoll.jsx
+++ src/components/PianoRoll.jsx
@@ -4,35 +4,10 @@ import classNames from 'classnames'
 import { whiteKey, OCTAVES } from '../globals'
 import './PianoRoll.scss'
 
-function noteToPx(note, after) {
-  let px = 0
-  const i = note % 12
-  const keyWidth = {
-    white: 13,
-    black: 8,
-  }
-  let afterPx = keyWidth.white
-  if (i <= 4) {
-    if (i % 2 === 0) {
-      px = (i / 2) * keyWidth.white
-    } else {
-      afterPx = keyWidth.black
-      px = keyWidth.white - keyWidth.black / 2 + keyWidth.white * Math.floor(i / 2)
-    }
-  } else {
-    if (i % 2 === 0) {
-      afterPx = keyWidth.black
-      px = keyWidth.white * 4 - keyWidth.black / 2 + keyWidth.white * ((i - 6) / 2)
-    } else {
-      px = ((i - 5) / 2 + 3) * keyWidth.white
-    }
-  }
-  px += Math.floor(note / 12) * keyWidth.white * 7
-  if (after) {
-    px += afterPx
-  }
-  return px
-}
+const CHANNEL_HEIGHT = 98
+const BLACK_KEY_HEIGHT = 58
+const BLACK_KEY_WIDTH = 8
+const WHITE_KEY_WIDTH = 13
 
 export default function PianoRoll({ playingNote, rangeStart, setRangeStart, rangeEnd, setRangeEnd }) {
   const pxStart = useMemo(() => noteToPx(rangeStart, false), [rangeStart])
@@ -49,7 +24,19 @@ export default function PianoRoll({ playingNote, rangeStart, setRangeStart, rang
             playing: playingNote === i,
           })}></div>
       ))}
-      <div style={{ left: pxStart - 2, width: pxEnd - pxStart - 2 }} className="piano-roll-range"></div>
+      <svg
+        style={{ left: pxStart.px - 2 }}
+        className="piano-roll-range"
+        width={pxEnd.px - pxStart.px + 2}
+        height="99"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d={`${noteLeftBoundary(pxStart.boundary, 1, CHANNEL_HEIGHT)} ${noteRightBoundary(
+            pxEnd.boundary,
+            pxEnd.px - pxStart.px + 1
+          )}`}
+        />
+      </svg>
     </div>
   )
 }
@@ -59,4 +46,63 @@ PianoRoll.propTypes = {
   setRangeStart: PropTypes.func,
   rangeEnd: PropTypes.number,
   setRangeEnd: PropTypes.func,
+}
+
+function noteLeftBoundary(i, x, height) {
+  switch (i) {
+    case 0:
+      return `M${x} 1 V ${height}`
+    case 1:
+      return `M${x} 1 V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / 2} V ${height}`
+    case 2:
+      return `M${x + BLACK_KEY_WIDTH / 2} 1 V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / -2} V ${height}`
+  }
+}
+
+function noteRightBoundary(i, x) {
+  switch (i) {
+    case 0:
+      return ` H ${x} V 1 Z`
+    case 1:
+      return ` H ${x} V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / -2} V 1 Z`
+    case 2:
+      return ` H ${x - BLACK_KEY_WIDTH / 2} V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / 2} V 1 Z`
+  }
+}
+
+function noteToPx(note, after) {
+  let px = 0
+  let boundary = 0
+  const i = note % 12
+  let afterPx = WHITE_KEY_WIDTH
+  if (i <= 4) {
+    if (!after && i > 0) {
+      boundary = i < 3 ? i : i - 2
+    } else if (after && i < 4) {
+      boundary = i % 2 === 0 ? 1 : 2
+    }
+    if (i % 2 === 0) {
+      px = (i / 2) * WHITE_KEY_WIDTH
+    } else {
+      afterPx = BLACK_KEY_WIDTH
+      px = WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2 + WHITE_KEY_WIDTH * Math.floor(i / 2)
+    }
+  } else {
+    if (!after && i > 5) {
+      boundary = i % 2 === 0 ? 1 : 2
+    } else if (after && i < 11) {
+      boundary = i % 2 === 0 ? 2 : 1
+    }
+    if (i % 2 === 0) {
+      afterPx = BLACK_KEY_WIDTH
+      px = WHITE_KEY_WIDTH * 4 - BLACK_KEY_WIDTH / 2 + WHITE_KEY_WIDTH * ((i - 6) / 2)
+    } else {
+      px = ((i - 5) / 2 + 3) * WHITE_KEY_WIDTH
+    }
+  }
+  px += Math.floor(note / 12) * WHITE_KEY_WIDTH * 7
+  if (after) {
+    px += afterPx
+  }
+  return { px, boundary }
 }
