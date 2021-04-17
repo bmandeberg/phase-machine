3912c3008265306b3f89d8e3b21e0e3ce1f9299c
Ben Mandeberg <bmandeberg@ucla.edu>
2021-04-16 23:44:59 -0700

    Fix range boundaries

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -112,26 +112,26 @@ export default function Piano({
         height="99"
         xmlns="http://www.w3.org/2000/svg">
         <path
-          d={`${noteLeftBoundary(pxStart.boundary, 1, CHANNEL_HEIGHT)} ${noteRightBoundary(
-            pxEnd.boundary,
+          d={`${noteLeftBoundary(pxStart.boundaryType, 1, CHANNEL_HEIGHT)} ${noteRightBoundary(
+            pxEnd.boundaryType,
             pxEnd.px - pxStart.px + 1
           )}`}
         />
       </svg>
       <div
         {...dragRangeLeft()}
-        style={{ left: pxStart.px - 6, width: pxStart.boundary ? 14 : 10 }}
+        style={{ left: pxStart.px - 6, width: pxStart.boundaryType ? 14 : 10 }}
         className="range-resize"></div>
       <div
         {...dragRange()}
         style={{
-          left: pxStart.px + (pxStart.boundary ? 8 : 4),
-          width: pxEnd.px - pxStart.px - 10 - (pxStart.boundary ? 4 : 0) - (pxEnd.boundary ? 4 : 0),
+          left: pxStart.px + (pxStart.boundaryType ? 8 : 4),
+          width: pxEnd.px - pxStart.px - 10 - (pxStart.boundaryType ? 4 : 0) - (pxEnd.boundaryType ? 4 : 0),
         }}
         className={classNames('range-drag', { grabbing, resizing })}></div>
       <div
         {...dragRangeRight()}
-        style={{ left: pxEnd.px - (pxEnd.boundary ? 10 : 6), width: pxEnd.boundary ? 14 : 10 }}
+        style={{ left: pxEnd.px - (pxEnd.boundaryType ? 10 : 6), width: pxEnd.boundaryType ? 14 : 10 }}
         className="range-resize"></div>
       <svg
         style={{ left: pxStart.px - 2 }}
@@ -149,8 +149,8 @@ export default function Piano({
           </filter>
         </defs>
         <path
-          d={`${noteLeftBoundary(pxStart.boundary, 1, CHANNEL_HEIGHT)} ${noteRightBoundary(
-            pxEnd.boundary,
+          d={`${noteLeftBoundary(pxStart.boundaryType, 1, CHANNEL_HEIGHT)} ${noteRightBoundary(
+            pxEnd.boundaryType,
             pxEnd.px - pxStart.px + 1
           )}`}
         />
@@ -171,27 +171,43 @@ Piano.propTypes = {
   noteOn: PropTypes.bool,
 }
 
-function noteLeftBoundary(i, x, height) {
-  switch (i) {
+function noteLeftBoundary(boundaryType, x, height) {
+  switch (boundaryType) {
     case 0:
       return `M${x} 1 V ${height}`
     case 1:
       return `M${x} 1 V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / 2} V ${height}`
     case 2:
       return `M${x + BLACK_KEY_WIDTH / 2} 1 V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / -2} V ${height}`
+    case 3:
+      return `M${x} 1 V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / 2 + 1} V ${height}`
+    case 4:
+      return `M${x + BLACK_KEY_WIDTH / 2 - 1} 1 V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / -2 + 1} V ${height}`
+    case 5:
+      return `M${x} 1 V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / 2 - 1} V ${height}`
+    case 6:
+      return `M${x + BLACK_KEY_WIDTH / 2 + 1} 1 V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / -2 - 1} V ${height}`
     default:
       return ''
   }
 }
 
-function noteRightBoundary(i, x) {
-  switch (i) {
+function noteRightBoundary(boundaryType, x) {
+  switch (boundaryType) {
     case 0:
       return ` H ${x} V 1 Z`
     case 1:
       return ` H ${x} V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / -2} V 1 Z`
     case 2:
       return ` H ${x - BLACK_KEY_WIDTH / 2} V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / 2} V 1 Z`
+    case 3:
+      return ` H ${x - BLACK_KEY_WIDTH / 2 + 1} V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / 2 - 1} V 1 Z`
+    case 4:
+      return ` H ${x} V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / -2 + 1} V 1 Z`
+    case 5:
+      return ` H ${x - BLACK_KEY_WIDTH / 2 - 1} V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / 2 + 1} V 1 Z`
+    case 6:
+      return ` H ${x} V ${BLACK_KEY_HEIGHT} h ${BLACK_KEY_WIDTH / -2 - 1} V 1 Z`
     default:
       return ''
   }
@@ -199,39 +215,44 @@ function noteRightBoundary(i, x) {
 
 function noteToPx(note, after) {
   let px = 0
-  let boundary = 0
+  let boundaryType = 0
   const i = note % 12
   let afterPx = WHITE_KEY_WIDTH
-  if (i <= 4) {
-    if (!after && i > 0) {
-      boundary = i < 3 ? i : i - 2
-    } else if (after && i < 4) {
-      boundary = i % 2 === 0 ? 1 : 2
+  if (i === 0 || i === 5) {
+    boundaryType = after ? 6 : 0
+    if (i === 5) {
+      px = WHITE_KEY_WIDTH * 3
     }
-    if (i % 2 === 0) {
-      px = (i / 2) * WHITE_KEY_WIDTH
-    } else {
-      afterPx = BLACK_KEY_WIDTH
-      px = WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2 + WHITE_KEY_WIDTH * Math.floor(i / 2)
-    }
-  } else {
-    if (!after && i > 5) {
-      boundary = i % 2 === 0 ? 1 : 2
-    } else if (after && i < 11) {
-      boundary = i % 2 === 0 ? 2 : 1
-    }
-    if (i % 2 === 0) {
-      afterPx = BLACK_KEY_WIDTH
-      px = WHITE_KEY_WIDTH * 4 - BLACK_KEY_WIDTH / 2 + WHITE_KEY_WIDTH * ((i - 6) / 2)
-    } else {
-      px = ((i - 5) / 2 + 3) * WHITE_KEY_WIDTH
-    }
+  } else if (i === 1 || i === 6) {
+    boundaryType = 3
+    afterPx = BLACK_KEY_WIDTH
+    px = WHITE_KEY_WIDTH * (i === 1 ? 1 : 4) - BLACK_KEY_WIDTH / 2 - 1
+  } else if (i === 2) {
+    boundaryType = 4
+    px = WHITE_KEY_WIDTH
+  } else if (i === 3 || i === 10) {
+    boundaryType = 5
+    afterPx = BLACK_KEY_WIDTH
+    px = WHITE_KEY_WIDTH * (i === 3 ? 2 : 6) - BLACK_KEY_WIDTH / 2 + 1
+  } else if (i === 4 || i === 11) {
+    boundaryType = after ? 0 : 6
+    px = WHITE_KEY_WIDTH * (i === 4 ? 2 : 6)
+  } else if (i === 7) {
+    boundaryType = after ? 1 : 4
+    px = WHITE_KEY_WIDTH * 4
+  } else if (i === 8) {
+    boundaryType = after ? 2 : 1
+    afterPx = BLACK_KEY_WIDTH
+    px = WHITE_KEY_WIDTH * 5 - BLACK_KEY_WIDTH / 2
+  } else if (i === 9) {
+    boundaryType = after ? 4 : 2
+    px = WHITE_KEY_WIDTH * 5
   }
   px += Math.floor(note / 12) * WHITE_KEY_WIDTH * 7
   if (after) {
     px += afterPx
   }
-  return { px, boundary }
+  return { px, boundaryType }
 }
 
 function keyOffset(x) {
